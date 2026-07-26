638442d96a531b0b924e646bdd2256c19044a3bb
nandeshkanagaraju <nandeshkanagaraju08@gmail.com>
2026-07-27 00:18:01 +0530

    Add Airflow Docker Compose setup and dependencies
    - Add 'apache-airflow>=3.3.0' to pyproject.toml and update lock file
    - Add airflow/ directory with docker-compose.yaml and configuration files
    - Integrate wallmart_project within airflow directory for docker volume mapping

---
+++ airflow/wallmart_project/models/silver_t/products_t.sql
@@ -0,0 +1,18 @@
+{{
+    config(
+        materialized = 'incremental',
+        unique_key = 'product_id',
+        schema = 'silver_t',
+        database = 'walmart'
+    )
+}}
+
+SELECT
+    *,
+    CURRENT_TIMESTAMP() AS processed_at
+FROM
+    {{ source('walmart_databricks', 'products') }}
+    
+{% if is_incremental()%}
+    WHERE updated_timestamp > (SELECT COALESCE(MAX(updated_timestamp), '1900-01-01') FROM {{this}})
+{% endif %} 
